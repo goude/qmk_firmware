288e4835bab4e6f41da9659ec58e71eec45f9f48
Daniel Goude <daniel@goude.se>
2020-04-11 18:29:58 +0200

    update keymap

--- keyboards/keebio/iris/keymaps/goude/README.md
+++ keyboards/keebio/iris/keymaps/goude/README.md
@@ -7,7 +7,7 @@ possibly with encoders replacing the upper thumb buttons (currently no encoders 
 
 Install the prerequisite packages like so:
 
-```
+```sh
 sudo utils/qmk_install.sh
 ```
 
@@ -17,16 +17,30 @@ avoid /usr/bin/pip3 warnings.
 
 ## Build Instructions
 
-```
+```sh
 make keebio/iris/rev4:goude
 ```
 
 ## Flash Instructions
 
-```
+```sh
 make keebio/iris/rev4:goude:flash
 ```
 
 Do this for one keyboard half at a time. Plug in the keyboard with a USB-C
 connector, run the flash command, press and hold the reset button for a few
 seconds until the keyboard is detected.
+
+## Notes
+
+Experience after performing a couple of `keymap.c` updates.
+
+On Ubuntu:
+
+```sh
+vim keymap.c
+sudo make keebio/iris/rev4:goude:flash
+# Press reset key combo
+```
+
+No need to flash halves separately when updating, it seems.
--- keyboards/keebio/iris/keymaps/goude/keymap.c
+++ keyboards/keebio/iris/keymaps/goude/keymap.c
@@ -16,38 +16,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // Main keyboard
   [_QWERTY] = LAYOUT(
-     KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_MINS,
-     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-     KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-     KC_GRAVE, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LOWER,              RAISE, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-                                    KC_RALT,  KC_LSPO, KC_BSPC,                   KC_SPC,  KC_RSPC,  KC_LCTL
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+    KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_GRAVE, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, LOWER,                 RAISE, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+                                    RALT_T(KC_LBRC),  KC_LSPO, KC_BSPC,                   KC_SPC,  KC_RSPC,  LCTL_T(KC_RBRC)
   ),
 
-  // Keypad, brackets, equal
+  // Cursor etc
   [_LOWER] = LAYOUT(
-     RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_EQUAL,
-     _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, KC_P7,    KC_P8,    KC_P9, XXXXXXX, XXXXXXX,
-     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC,                            KC_RBRC, KC_P4,   KC_P5,   KC_P6,   XXXXXXX, XXXXXXX,
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, _______,          _______, KC_RCBR, KC_P1,   KC_P2,   KC_P3,   XXXXXXX, XXXXXXX,
-                                    _______, _______, _______,                   _______, _______, KC_P0
+    RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, KC_EQUAL,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, XXXXXXX,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,          _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,
+                                    _______, _______, _______,                   _______, _______, _______
   ),
 
-  // Single hand typing
+  // Single right hand typing
   [_RAISE] = LAYOUT(
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_EQUAL,
-     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,
-     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX,
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,          _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_RALT,
-                                    KC_LALT, KC_LGUI,  KC_DEL,                   XXXXXXX, XXXXXXX, XXXXXXX
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_EQUAL,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    OSM(KC_RALT),
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,          _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    OSM(KC_RSHIFT),
+                                    KC_LALT, KC_LGUI,  KC_DEL,                   LOWER, XXXXXXX, XXXXXXX
   ),
 
   // Function keys
   [_ADJUST] = LAYOUT(
-     KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,          _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                    XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX
+    KC_F12,  KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,
+    _______, RGB_TOG, RGB_MOD, RGB_RMOD, XXXXXXX, XXXXXXX,                            KC_TAB,  KC_PGDN, KC_PGUP, XXXXXXX,  XXXXXXX, KC_BSPC,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX, OSM(KC_RCTRL),
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, _______,          _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, OSM(KC_RSHIFT),
+                                    XXXXXXX, XXXXXXX, XXXXXXX,                   _______, XXXXXXX, XXXXXXX
   )
 };
 
